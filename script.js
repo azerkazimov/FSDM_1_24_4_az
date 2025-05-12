414776e567bc62c2af1fb4d30b3beb83c448f087
Azer Kazimov <azer.kazimov@yahoo.com>
2025-05-12 11:48:24 +0400

    init

---
+++ script.js
@@ -0,0 +1,43 @@
+//  ==== VAR ====
+
+// console.log(greeting); // undefined
+
+// greeting
+
+// console.log(greeting); // undefined
+
+// var greeting = "Hello, World!";
+// // console.log(greeting); // "Hello, World!"
+
+
+// var greeting = "Hello, Universe!";
+// var greeting =76876687678678
+// // console.log(greeting); // 76876687678678
+// greeting = "necesiz";
+
+// console.log(greeting); // "necesiz"
+
+// ==== LET ====
+
+
+let  greeting = "Hello, World!";
+greeting = "Hello, Universe!";
+
+console.log(greeting);
+
+
+// ==== CONST ====
+const greet = "Hello, World!";
+// greet = "Hello, Universe!"; // TypeError: Assignment to constant variable.
+console.log(greet); // "Hello, World!"
+
+
+
+
+
+
+
+
+ 
+
+
